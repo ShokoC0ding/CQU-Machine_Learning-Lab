eba1e5ff5d249e034f0559d7295233c346b9456c
Shoko <leirishyoko@gmail.com>
2024-10-29 14:46:52 +0800

    Add files via upload

---
+++ 决策树/tree/C4.5Tree.py
@@ -0,0 +1,382 @@
+# CART决策树，使用基尼指数（Gini index）来选择划分属性
+# 分别实现预剪枝、后剪枝和不进行剪枝的实现
+
+import math
+from lib.lib import TreeNode
+from lib.lib import watermelon2
+import graphviz
+
+
+
+def is_number(s):
+    """判断一个字符串是否为数字，如果是数字，我们认为这个属性的值是连续的"""
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    return False
+
+
+def ent(labels=[]):
+    """
+    计算数据集的基尼值
+    :param labels: 数据集的类别标签
+    :return:
+    """
+    label_name=[]
+    label_count=[]
+    entropy = 0
+
+
+    for item in labels:
+        if not (item in label_name):
+            label_name.append(item)
+            label_count.append(1)
+        else:
+            index=label_name.index(item)
+            label_count[index] +=1
+
+    n=sum(label_count)
+    entropy=0.0
+    for item in label_count:
+        p=item/n
+        entropy = entropy-p*math.log(p,2)
+
+    return entropy
+
+
+def gain(attribute, labels, is_value=False):
+    """
+    计算信息增益
+    :param attribute: 集合中样本该属性的值列表
+    :param labels: 集合中样本的数据标签
+    :return:
+    """
+    # is_value = False  # 当前属性是离散的形容词还是连续的数值
+    info_gain = ent(labels)
+    n = len(labels)
+    split_value = None  # 如果是连续值的话，也需要返回分隔界限的值
+
+    if is_value:
+        # print('attribute', attribute)
+        # 属性值是连续的数值，首先应该使用二分法寻找最佳分割点
+        sorted_attribute = attribute.copy()
+        sorted_attribute.sort()
+        split = []  # 候选的分隔点
+        for i in range(0, n - 1):
+            temp = (sorted_attribute[i] + sorted_attribute[i + 1]) / 2
+            split.append(temp)
+        info_gain_list = []
+        # print('split', split)
+        for temp_split in split:
+            low_labels = []
+            high_labels = []
+            for i in range(0, n):
+                if attribute[i] <= temp_split:
+                    low_labels.append(labels[i])
+                else:
+                    high_labels.append(labels[i])
+            temp_gain = info_gain - len(low_labels) / n * ent(low_labels) - len(high_labels) / n * ent(high_labels)
+            info_gain_list.append(temp_gain)
+
+        # print('info_gain_list', info_gain_list)
+        info_gain = max(info_gain_list)
+        max_index = info_gain_list.index(info_gain)
+        split_value = split[max_index]
+    else:
+        # 属性值是离散的值
+        attribute_dict = {}
+        label_dict = {}
+        index = 0
+        for item in attribute:
+            if attribute_dict.__contains__(item):
+                attribute_dict[item] = attribute_dict[item] + 1
+                label_dict[item].append(labels[index])
+            else:
+                attribute_dict[item] = 1
+                label_dict[item] = [labels[index]]
+            index += 1
+
+        for key, value in attribute_dict.items():
+            info_gain = info_gain - value / n * ent(label_dict[key])
+
+    return info_gain, split_value
+
+
+def finish_node(current_node, data, label):
+    """
+    完成当前结点的后续计算，包括选择属性，划分子节点等
+    :param current_node: 当前的结点
+    :param data: 数据集
+    :param label: 数据集的 label
+    :param rest_title: 剩余的可用属性名
+    :return:
+    """
+    n = len(label)
+
+    # 判断当前结点的数据是否属于同一类，如果是，直接标记为叶子结点并返回
+    one_class = True
+
+    this_data_index = current_node.data_index
+    for i in this_data_index:
+        for j in this_data_index:
+            if label[i] != label[j]:
+                one_class = False
+                break
+        if not one_class:
+            break
+    if one_class:
+        current_node.judge = label[this_data_index[0]]
+        print(str(current_node.parent.attribute_name)+': '+str(current_node.attribute_value))
+        print('包含的数据索引:'+str(current_node.data_index)+'均属于同一标签，是叶子结点')
+        return
+
+    rest_title = current_node.rest_attribute  # 候选属性
+    if len(rest_title) == 0:  # 如果候选属性为空，则是个叶子结点。需要选择最多的那个类作为该结点的类
+        label_count = {}
+        temp_data = current_node.data_index
+        for index in temp_data:
+            if label_count.__contains__(label[index]):
+                label_count[label[index]] += 1
+            else:
+                label_count[label[index]] = 1
+        final_label = max(label_count)
+        current_node.judge = final_label
+        print('包含的数据索引:'+str(current_node.data_index)+'候选属性为空，是叶子结点')
+        return
+
+    title_gain = {}  # 记录每个属性的信息增益
+    title_split_value = {}  # 记录每个属性的分隔值，如果是连续属性则为分隔值，如果是离散属性则为None
+    title_IV = {}
+    for title in rest_title:
+        attr_values = []
+        current_label = []
+        for index in current_node.data_index:
+            this_data = data[index]
+            attr_values.append(this_data[title])
+            current_label.append(label[index])
+        temp_data = data[0]
+        this_gain, this_split_value = gain(attr_values, current_label, is_number(temp_data[title]))  # 如果属性值为数字，则认为是连续的
+        title_IV[title] = ent(attr_values)
+        title_gain[title] = this_gain
+        title_split_value[title] = this_split_value
+
+    gain_list  = title_gain.values()
+    ave_gain = sum(gain_list)/len(gain_list)
+
+    above_average_pairs = {k: v for k, v in title_gain.items() if v > ave_gain}
+
+    gain_ratio_dict = {}
+    for key in above_average_pairs:
+        if key in title_IV:
+            gain_ratio = above_average_pairs[key]/title_IV[key]
+            gain_ratio_dict[key] = gain_ratio
+
+    best_attr = max(gain_ratio_dict, key=gain_ratio_dict.get)  # 信息增益最大的属性名
+    current_node.attribute_name = best_attr
+    current_node.split = title_split_value[best_attr]
+    rest_title.remove(best_attr)
+
+    a_data = data[0]
+    if is_number(a_data[best_attr]):  # 如果是该属性的值为连续数值
+        split_value = title_split_value[best_attr]
+        small_data = []
+        large_data = []
+        for index in current_node.data_index:
+            this_data = data[index]
+            if this_data[best_attr] <= split_value:
+                small_data.append(index)
+            else:
+                large_data.append(index)
+        small_str = '<=' + str(split_value)
+        large_str = '>' + str(split_value)
+        small_child = TreeNode(parent=current_node, data_index=small_data, attr_value=small_str,
+                               rest_attribute=rest_title.copy())
+        large_child = TreeNode(parent=current_node, data_index=large_data, attr_value=large_str,
+                               rest_attribute=rest_title.copy())
+        current_node.children = [small_child, large_child]
+
+    else:  # 如果该属性的值是离散值
+        best_titlevalue_dict = {}  # key是属性值的取值，value是个list记录所包含的样本序号
+        for index in current_node.data_index:
+            this_data = data[index]
+            if best_titlevalue_dict.__contains__(this_data[best_attr]):
+                temp_list = best_titlevalue_dict[this_data[best_attr]]
+                temp_list.append(index)
+            else:
+                temp_list = [index]
+                best_titlevalue_dict[this_data[best_attr]] = temp_list
+
+        children_list = []
+        for key, index_list in best_titlevalue_dict.items():
+            a_child = TreeNode(parent=current_node, data_index=index_list, attr_value=key,
+                               rest_attribute=rest_title.copy())
+            children_list.append(a_child)
+        current_node.children = children_list
+
+    # print(current_node.to_string())
+    for child in current_node.children:  # 递归
+        finish_node(child, data, label)
+
+
+def id3_tree(Data, title, label):
+    """
+    id3方法构造决策树，使用的标准是信息增益（信息熵）
+    :param Data: 数据集，每个样本是一个 dict(属性名：属性值)，整个 Data 是个大的 list
+    :param title: 每个属性的名字，如 色泽、含糖率等
+    :param label: 存储的是每个样本的类别
+    :return:
+    """
+    n = len(Data)
+    rest_title = title.copy()
+    root_data = []
+    for i in range(0, n):
+        root_data.append(i)
+
+    root_node = TreeNode(data_index=root_data, rest_attribute=title.copy())
+    finish_node(root_node, Data, label)
+
+    return root_node
+
+
+def print_tree(root=TreeNode()):
+    """
+    打印输出一颗树
+    :param root: 根节点
+    :return:
+    """
+    node_list = [root]
+    while (len(node_list) > 0):
+        current_node = node_list[0]
+        print('--------------------------------------------')
+        print(current_node.to_string())
+        print('--------------------------------------------')
+        children_list = current_node.children
+        if not (children_list is None):
+            for child in children_list:
+                node_list.append(child)
+        node_list.remove(current_node)
+
+
+def print_tree(root):
+    """
+    打印输出一颗树
+    :param root: 根节点
+    :return:
+    """
+    node_list = [root]
+    while (len(node_list) > 0):
+        current_node = node_list[0]
+        print('--------------------------------------------')
+        print(current_node.to_string())
+        print(current_node.print_dot())
+        print('--------------------------------------------')
+        children_list = current_node.children
+        if not (children_list is None):
+            for child in children_list:
+                node_list.append(child)
+        node_list.remove(current_node)
+
+def dot_graph(root):
+    string = []
+    node_list = [root]
+    Note = open('../test.dot', mode='w')
+    Note.write('digraph G {	node [fontname=FangSong];edge[fontname=FangSong];')
+
+    while (len(node_list) > 0):
+        current_node = node_list[0]
+#        string.append(current_node.print_dot())
+        Note = open('../test.dot', mode='a')
+        Note.write(current_node.print_dot())
+        children_list = current_node.children
+        if not (children_list is None):
+            for child in children_list:
+                node_list.append(child)
+        node_list.remove(current_node)
+
+    Note = open('../test.dot', mode='a')
+    Note.write('}')
+
+def dot_graph_draw():
+    with open('../test.dot', encoding='gbk') as fj:
+        source = fj.read()
+
+    dot = graphviz.Source(source)
+    dot.view()
+
+def classify_data(decision_tree, x={}):
+    """
+    使用决策树判断一个数据样本的类别标签
+    :param decision_tree: 决策树的根节点
+    :param x: 要进行判断的样本
+    :return:
+    """
+    current_node = decision_tree
+    while current_node.judge is None:
+        if current_node.split is None:  # 离散属性
+            can_judge = False  # 如果训练数据集不够大，测试数据集中可能会有在训练数据集中没有出现过的属性值
+            for child in current_node.children:
+                if child.attribute_value == x[current_node.attribute_name]:
+                    current_node = child
+                    can_judge = True
+                    break
+            if not can_judge:
+                return None
+        else:
+            child_list = current_node.children
+            if x[current_node.attribute_name] <= current_node.split:
+                current_node = child_list[0]
+            else:
+                current_node = child_list[1]
+
+    return current_node.judge
+
+
+def run_test():
+    train_watermelon, test_watermelon, title = watermelon2()
+
+    # 先处理数据
+    train_data = []
+    test_data = []
+    train_label = []
+    test_label = []
+
+    for melon in train_watermelon:
+        a_dict = {}
+        dim = len(melon) - 1
+        for i in range(0, dim):
+            a_dict[title[i]] = melon[i]
+        train_data.append(a_dict)
+        train_label.append(melon[dim])
+
+    for melon in test_watermelon:
+        a_dict = {}
+        dim = len(melon) - 1
+        for i in range(0, dim):
+            a_dict[title[i]] = melon[i]
+        test_data.append(a_dict)
+        test_label.append(melon[dim])
+
+    decision_tree = id3_tree(train_data, title, train_label)
+    print_tree(decision_tree)
+    dot_graph(decision_tree)
+    dot_graph_draw()
+
+    test_judge = []
+    for melon in test_data:
+        test_judge.append(classify_data(decision_tree, melon))
+    print('决策树在测试数据集上的分类结果是：', test_judge)
+    print('测试数据集的正确类别信息应该是：  ', test_label)
+
+    accuracy = 0
+    for i in range(0, len(test_label)):
+        if test_label[i] == test_judge[i]:
+            accuracy += 1
+    accuracy /= len(test_label)
+    print('决策树在测试数据集上的分类正确率为：' + str(accuracy * 100) + "%")
+
+
+
+if __name__ == '__main__':
+    run_test()
---
+++ 决策树/tree/cartTree_precut.py
@@ -0,0 +1,273 @@
+# 预剪枝的CART决策树
+from lib.lib import TreeNode
+from lib.lib import watermelon2
+import cartTree
+import graphviz
+
+
+def current_accuracy(tree_node=TreeNode, test_data=[], test_label=[]):
+    """
+    计算当前决策树在训练数据集上的正确率
+    :param tree_node: 要判断的决策树结点
+    :param test_data: 测试数据集
+    :param test_label: 测试数据集的label
+    :return:
+    """
+    root_node = tree_node
+    while not root_node.parent is None:
+        root_node = root_node.parent
+
+    accuracy = 0
+    for i in range(0, len(test_label)):
+        this_label = cartTree.classify_data(root_node, test_data[i])
+        if this_label == test_label[i]:
+            accuracy += 1
+    # print(str(tree_node.index) + " 处，分对了"+str(accuracy))
+    return accuracy / len(test_label)
+
+
+def finish_node(current_node, data=[], label=[], test_data=[], test_label=[]):
+    """
+    完成一个结点上的计算
+    :param current_node: 当前计算的结点
+    :param data: 数据集
+    :param label: 数据集的 label
+    :param test_data: 测试数据集
+    :param test_label: 测试数据集的label
+    :return:
+    """
+    n = len(label)
+
+    # 判断当前结点中的数据是否属于同一类
+    one_class = True
+    this_data_index = current_node.data_index
+
+    for i in this_data_index:
+        for j in this_data_index:
+            if label[i] != label[j]:
+                one_class = False
+                break
+        if not one_class:
+            break
+    if one_class:
+        current_node.judge = label[this_data_index[0]]
+        return
+
+    rest_title = current_node.rest_attribute  # 候选属性
+    if len(rest_title) == 0:  # 如果候选属性为空，则是个叶子结点。需要选择最多的那个类作为该结点的类
+        label_count = {}
+        temp_data = current_node.data_index
+        for index in temp_data:
+            if label_count.__contains__(label[index]):
+                label_count[label[index]] += 1
+            else:
+                label_count[label[index]] = 1
+        final_label = max(label_count)
+        current_node.judge = final_label
+        return
+
+    # 先为当前结点添加一个临时判断，如果需要添加孩子结点，就再把它恢复成None
+    data_count = {}
+    for index in current_node.data_index:
+        if data_count.__contains__(label[index]):
+            data_count[label[index]] += 1
+        else:
+            data_count[label[index]] = 1
+    before_judge = max(data_count, key=data_count.get)
+    current_node.judge = before_judge
+    before_accuracy = current_accuracy(current_node, test_data, test_label)
+
+    title_gini = {}  # 记录每个属性的基尼指数
+    title_split_value = {}  # 记录每个属性的分隔值，如果是连续属性则为分隔值，如果是离散属性则为None
+    for title in rest_title:
+        attr_values = []
+        current_label = []
+        for index in current_node.data_index:
+            this_data = data[index]
+            attr_values.append(this_data[title])
+            current_label.append(label[index])
+        temp_data = data[0]
+        this_gain, this_split_value = cartTree.gini_index(attr_values, current_label,
+                                                          cartTree.is_number(temp_data[title]))  # 如果属性值为数字，则认为是连续的
+        title_gini[title] = this_gain
+        title_split_value[title] = this_split_value
+
+    best_attr = min(title_gini, key=title_gini.get)  # 基尼指数最小的属性名
+    current_node.attribute_name = best_attr
+    current_node.split = title_split_value[best_attr]
+    rest_title.remove(best_attr)
+
+    a_data = data[0]
+    if cartTree.is_number(a_data[best_attr]):  # 如果是该属性的值为连续数值
+        split_value = title_split_value[best_attr]
+        small_data = []
+        large_data = []
+        for index in current_node.data_index:
+            this_data = data[index]
+            if this_data[best_attr] <= split_value:
+                small_data.append(index)
+            else:
+                large_data.append(index)
+        small_str = '<=' + str(split_value)
+        large_str = '>' + str(split_value)
+        small_child = TreeNode(parent=current_node, data_index=small_data, attr_value=small_str,
+                                        rest_attribute=rest_title.copy())
+        large_child = TreeNode(parent=current_node, data_index=large_data, attr_value=large_str,
+                                        rest_attribute=rest_title.copy())
+        # 也需要先给子节点一个判断
+        small_data_count = {}
+        for index in small_child.data_index:
+            if small_data_count.__contains__(label[index]):
+                small_data_count[label[index]] += 1
+            else:
+                small_data_count[label[index]] = 1
+        small_child_judge = max(small_data_count, key=small_data_count.get)
+        small_child.judge = small_child_judge  # 临时添加的一个判断
+        large_data_count = {}
+        for index in large_child.data_index:
+            if large_data_count.__contains__(label[index]):
+                large_data_count[label[index]] += 1
+            else:
+                large_data_count[label[index]] = 1
+        large_child_judge = max(large_data_count, key=large_data_count.get)
+        large_child.judge = large_child_judge  # 临时添加的一个判断
+        current_node.children = [small_child, large_child]
+
+    else:  # 如果该属性的值是离散值
+        best_titlevalue_dict = {}  # key是属性值的取值，value是个list记录所包含的样本序号
+        for index in current_node.data_index:
+            this_data = data[index]
+            if best_titlevalue_dict.__contains__(this_data[best_attr]):
+                temp_list = best_titlevalue_dict[this_data[best_attr]]
+                temp_list.append(index)
+            else:
+                temp_list = [index]
+                best_titlevalue_dict[this_data[best_attr]] = temp_list
+
+        children_list = []
+        for key, index_list in best_titlevalue_dict.items():
+            a_child = TreeNode(parent=current_node, data_index=index_list, attr_value=key,
+                                        rest_attribute=rest_title.copy())
+            # 也需要先给子节点一个判断
+            temp_data_count = {}
+            for index in index_list:
+                if temp_data_count.__contains__(label[index]):
+                    temp_data_count[label[index]] += 1
+                else:
+                    temp_data_count[label[index]] = 1
+            temp_child_judge = max(temp_data_count, key=temp_data_count.get)
+            a_child.judge = temp_child_judge  # 临时添加的一个判断
+            children_list.append(a_child)
+        current_node.children = children_list
+
+    current_node.judge = None
+    later_accuracy = current_accuracy(current_node, test_data, test_label)
+    # print(str(current_node.index)+"处，不剪枝的正确率是 "+str(later_accuracy) +"，剪枝的正确率是 "+str(before_accuracy))
+    print('当前节点: '+current_node.attribute_name)
+    print('剪枝前: '+str(before_accuracy))
+    print('剪枝后: '+str(later_accuracy))
+    if before_accuracy > later_accuracy:
+        current_node.children = None
+        current_node.judge = before_judge
+        # print(str(current_node.index)+"处进行剪枝")
+        return
+    else:
+        # print(current_node.to_string())
+        for child in current_node.children:  # 递归
+            finish_node(child, data, label, test_data, test_label)
+
+
+def precut_cart_tree(Data, title, label, test_data, test_label):
+    """
+    生成一颗预剪枝 CART 决策树
+    :param Data: 训练数据集，每个样本是一个 dict(属性名：属性值)，整个 Data 是个大的 list
+    :param title:   每个属性的名字，如 色泽、含糖率等
+    :param label: 存储的是训练集每个样本的类别
+    :param test_data: 测试数据集
+    :param test_label: 测试数据集的标签
+    :return:
+    """
+    n = len(Data)
+    rest_title = title.copy()
+    root_data = []
+    for i in range(0, n):
+        root_data.append(i)
+
+    root_node = TreeNode(data_index=root_data, rest_attribute=title.copy())
+    finish_node(root_node, Data, label, test_data, test_label)
+
+    return root_node
+
+def dot_graph(root):
+    string = []
+    node_list = [root]
+    Note = open('../test.dot', mode='w')
+    Note.write('digraph G {	node [fontname=FangSong];edge[fontname=FangSong];')
+
+    while (len(node_list) > 0):
+        current_node = node_list[0]
+#        string.append(current_node.print_dot())
+        Note = open('../test.dot', mode='a')
+        Note.write(current_node.print_dot())
+        children_list = current_node.children
+        if not (children_list is None):
+            for child in children_list:
+                node_list.append(child)
+        node_list.remove(current_node)
+
+    Note = open('../test.dot', mode='a')
+    Note.write('}')
+
+def dot_graph_draw():
+    with open('../test.dot', encoding='gbk') as fj:
+        source = fj.read()
+
+    dot = graphviz.Source(source)
+    dot.view()
+
+def run_test():
+    train_watermelon, test_watermelon, title = watermelon2()
+
+    # 先处理数据
+    train_data = []
+    test_data = []
+    train_label = []
+    test_label = []
+    for melon in train_watermelon:
+        a_dict = {}
+        dim = len(melon) - 1
+        for i in range(0, dim):
+            a_dict[title[i]] = melon[i]
+        train_data.append(a_dict)
+        train_label.append(melon[dim])
+    for melon in test_watermelon:
+        a_dict = {}
+        dim = len(melon) - 1
+        for i in range(0, dim):
+            a_dict[title[i]] = melon[i]
+        test_data.append(a_dict)
+        test_label.append(melon[dim])
+
+    decision_tree = precut_cart_tree(train_data, title, train_label, test_data, test_label)
+    print('训练的决策树是:')
+    cartTree.print_tree(decision_tree)
+    dot_graph(decision_tree)
+    dot_graph_draw()
+    print('\n')
+
+    test_judge = []
+    for melon in test_data:
+        test_judge.append(cartTree.classify_data(decision_tree, melon))
+    print('决策树在测试数据集上的分类结果是：', test_judge)
+    print('测试数据集的正确类别信息应该是：  ', test_label)
+
+    accuracy = 0
+    for i in range(0, len(test_label)):
+        if test_label[i] == test_judge[i]:
+            accuracy += 1
+    accuracy /= len(test_label)
+    print('决策树在测试数据集上的分类正确率为：' + str(accuracy * 100) + "%")
+
+
+if __name__ == '__main__':
+    run_test()
